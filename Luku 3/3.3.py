051a534cca883c010cdedfa7761c2d51d3f884ca
Eelis Soikkeli <eelis.soikkeli@gmail.com>
2023-09-14 18:21:47 +0300

    Initial commit

--- Luku 3/3.3.py
+++ Luku 3/3.3.py
@@ -1,16 +1,38 @@
+def get_valid_input(prompt):
+    loop = True
+    while loop:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input.")
+
 def tracker():
     runs = []
-    days = int(input("Enter the number of days: "))
-    if days > 0:
-        for day in range(1, days+1):
-            length = int(input(f"Enter day {day} length: "))
-            if length > 0:
-                runs.append(length)
-            else:
-                print("Run length can't be less than zero.")
-        runs.sort()
-        print(runs)
+    threshold = 0
+    count = 0
+    days = get_valid_input("Enter the number of days: ")
+    for day in range(1, days+1):
+        length = get_valid_input(f"Enter day {day} running length: ")
+        if length <= threshold:
+            count += 1
+            if count >= 3:
+                print("You had too many consecutive lazy days!")
+                exit()
+        else:
+            count = 0
+        runs.append(length)
+
+    runs.sort()
+    if days % 2 == 1:
+        median = runs[days // 2]
     else:
-        print("Days can't be zero or less.")
+        middle1 = runs[days // 2 - 1]
+        middle2 = runs[days // 2]
+        median = (middle1 + middle2) / 2
+    if median < 3:
+        print(f"Your running mean of {median} km was too low!")
+    else:
+        print(f"You were persistent runner! With a mean of {median} km.")
 
 tracker()
