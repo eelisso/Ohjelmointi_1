00a3d4134300b75b8883798dbc7f0e74dd5f4736
Eelis Soikkeli <eelis.soikkeli@gmail.com>
2023-09-14 23:18:13 +0300

    Updated

--- Luku 3/3.3.py
+++ Luku 3/3.3.py
@@ -3,12 +3,13 @@ Eelis Soikkeli, tehtävä 3.3
 """
 
 
-def get_valid_input(data_type, prompt):  # Validify user input, take in datatype and input prompt as parameters
+def get_valid_input(data_type, prompt):
+    """ Validate user input, take in datatype and input prompt as parameters """
     loop = True
     while loop:  # Create loop for input validation
         try:
             value = data_type(input(prompt).replace(",", "."))
-            if value > 0:
+            if value >= 0:
                 return value  # Return input value and break loop if input is correct
             else:
                 print("Invalid input.")
@@ -17,7 +18,10 @@ def get_valid_input(data_type, prompt):  # Validify user input, take in datatype
             print("Invalid input.")  # Print out an error and prompt the user again for correct input
 
 
-def tracker():  # Get user input on number of tracked days and distance ran each day
+count = 0  # Tracker for amount of consecutive "lazy days"
+
+def tracker():
+    """ Get user input on number of tracked days and distance ran each day """
     threshold = 0  # Set threshold for "lazy days"
     count = 0  # Tracker for amount of consecutive "lazy days"
     days = get_valid_input(int, "Enter the number of days: ")
@@ -26,27 +30,31 @@ def tracker():  # Get user input on number of tracked days and distance ran each
         if length <= threshold:
             count += 1
             if count >= 3:
-                print("You had too many consecutive lazy days!")
-                exit()  # Exit the program
+                print("\nYou had too many consecutive lazy days!")
+                break  # Exit the program
         else:
             count = 0
         runs.append(length)  # Add each iteration to a list called "runs"
 
 
-def median_calc():
-    runs.sort()  #
-    if len(runs) % 2 != 0:  # Odd number of days
-        median = runs[len(runs) // 2 + 1]
-    else:  # Even number of days
-        middle1 = runs[len(runs) // 2 - 1]
-        middle2 = runs[len(runs) // 2]
-        median = (middle1 + middle2) / 2
-    if median < 3:
-        print(f"Your running mean of {median:.2f} km was too low!")
-    else:
-        print(f"You were persistent runner! With a mean of {median} km.")
+def mean_calc():
+    """ Calculate the mean of distance ran """
+    if count > 3:
+        total = 0
+        for a in runs:
+            total += a
+        mean = total / len(runs)
+        if mean < 3:
+            print(f"\nYour running mean of {mean:.2f} km was too low!")
+        else:
+            print(f"\nYou were persistent runner! With a mean of {mean:.2f} km.")
 
 
 runs = []
-tracker()
-median_calc()
+
+
+def main():
+    tracker()
+    mean_calc()
+
+main()
